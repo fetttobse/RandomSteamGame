bcc24c6c000a3df06970c152049d52f5f1756aed
Tobias Lachmuth <Tobias Lachmuth>
2016-12-08 12:11:05 +0100

    Implemented XHR in JS, issues pending.

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,17 @@
 document.getElementById("randomBtn").addEventListener("click", function () {
     //On Click get random Steam Game
     console.log("Button clicked");
+
+    //Create an instance of XMLHttpRequest
+    var request = new XMLHttpRequest();
+    console.log("request instance created");
+
+    request.open("GET", "http://api.steampowered.com/ISteamApps/GetAppList/v0001/");
+    console.log("request opened");
+    request.setRequestHeader("Origin","");
+    console.log("request Header set");
+    request.send();
+    console.log("request sent");
+
+    console.log(request.responseText);
 });
